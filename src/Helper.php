0c2819d14f99903eb8ed8922c63391a69028bad8
宋明屿 <384971424@qq.com>
2022-04-08 15:00:04 +0800

    -

--- src/Helper.php
+++ src/Helper.php
@@ -1,10 +1,14 @@
 <?php 
+/**
+ * 帮助函数
+ */
+namespace Zzan\Pwfhlr;
 
 /**
  * 封装 json_encode(不进行unicode编码)
  * @return json
  */
-if (! function_exists('Json')) {
+if (! function_exists('Zzan\Pwfhlr\Json')) {
 
     function Json($arr) {
         return json_encode($arr, JSON_UNESCAPED_UNICODE);
